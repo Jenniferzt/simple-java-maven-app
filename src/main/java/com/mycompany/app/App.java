c1bdd95df0a2408a7a578ce3a7865d8f1da0456b
wangyingwei <wangyingwei120@163.com>
2018-04-21 18:47:58 +0800

    second commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World11111";
+    private final String message = "Hello World wyw";
 
     public App() {}
 
